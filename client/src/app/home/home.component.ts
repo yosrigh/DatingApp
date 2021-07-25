8ab116a0caf9e53ada7acb90b5da8181e5904797
yosrigh <33942982+yosrigh@users.noreply.github.com>
2021-07-25 16:53:55 +0100

    add register and login

---
+++ client/src/app/home/home.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent implements OnInit {
+  registerMode = false;
+  users: any;
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+  
+  regitserToggle(){
+    this.registerMode = !this.registerMode;
+  }
+
+  public cancelRegisterMode(event: boolean){
+    this.registerMode = event;
+  }
+}
